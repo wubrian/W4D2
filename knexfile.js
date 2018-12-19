88ed02dea49d600caac25c73c6ac4bd3457651a5
Brian Wu <wubrian1234@hotmail.com>
2018-12-19 03:07:34 +0000

    implement up and down for kexfile to add table and rollback

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database,
+      host     : settings.hostname,
+      port     : settings.port,
+      ssl      : settings.ssl
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
