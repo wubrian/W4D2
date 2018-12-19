a7da522a41f94bdba6a089ffba29b2aa4d74cdcc
Brian Wu <wubrian1234@hotmail.com>
2018-12-19 03:34:33 +0000

    add migration script version 2

--- migrations/20181219023633_create_milestones.js
+++ migrations/20181219023633_create_milestones.js
@@ -1,16 +1,31 @@
 
-exports.up = function(knex, Promise) {  
-    return Promise.all([
-      knex.schema.createTable('milestones', function(table){
-        table.increments();
-        table.string("description");
-        table.date("date_achieved");
-      })
-    ])
-  };
+// exports.up = function(knex, Promise) {  
+//     return Promise.all([
+//       knex.schema.createTable('milestones', function(table){
+//         table.increments();
+//         table.string("description");
+//         table.date("date_achieved");
+//       })
+//     ])
+//   };
 
-exports.down = function(knex, Promise) {  
+// exports.down = function(knex, Promise) {  
+//     return Promise.all([
+//       knex.schema.dropTable('milestones')
+//     ])
+//   };
+
+exports.up = function(knex, Promise) {
     return Promise.all([
-      knex.schema.dropTable('milestones')
-    ])
-  };
+        knex.schema.table('milestones', function(table){
+            table.integer('famous_person_id');
+            table.foreign('famous_person_id').references('famous_people.id');
+        })
+      ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('milestones')
+      ])
+};
